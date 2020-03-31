73b0df31ea036bf5b1057e9703b4351474317a21
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-03-31 07:13:33 +0300

    Published Maven project

---
+++ src/main/java/com/softserve/itacademy/App.java
@@ -0,0 +1,14 @@
+package com.softserve.itacademy;
+
+import org.joda.time.LocalDateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
+}
